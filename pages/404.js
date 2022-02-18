c7ca254075d7bbea66efd8a9140aea6aadd39613
Gaffyn Voorzta <gaffyn.voorzta@hotmail.com>
2022-02-18 19:37:30 +0000

    404...

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,16 @@
 // pages/404.js
+import styles from '../components/layout.module.css'
+import Link from 'next/link'
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found. Gutted.</h1>
+    return (
+        <>
+        <h1>404 - Page Not Found. Gutted.</h1>
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+        </>
+    )
   }
