30e03bdd311c9c1231fb83be3ef417d5927ac155
Gaffyn Voorzta <gaffyn.voorzta@hotmail.com>
2022-02-21 21:27:01 +0000

    update api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,12 @@
 export default function handler(req, res) {
     const msg = [ 'Well hello there.', 'Huh?', 'Eh?',
     'Almost an API', 'Kippers', 'Flanges', 'Spinach',
-    'Dat sick blud!', 'Wha...Me?' ]
+    'Dat sick blud!', 'Wha...Me?', 'Thank you for shopping with us',
+    'Please click ok to continue',
+    'Tell you what, chuck in some apples as well and call it a Fiver!',
+    'Dinners Ready!!!',
+    'Have you tidied your bedroom?',
+    'The meeting will take place as normal. Make sure you aint followed...' ]
     const idx = Math.floor(Math.random() * msg.length)
     const randmsg = msg[idx]
     res.status(200).json({ text: randmsg })
